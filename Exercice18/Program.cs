61f81cd7147130db931abffe5008b2d93dc41b00
celie <celie@DESKTOP-7CTHT5D>
2023-01-26 21:52:27 +0100

    Ajoutez des fichiers projet.

---
+++ Exercice18/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, petit;
+
+            Console.WriteLine("Saisissez un nombre (1) : ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Saisissez un nombre (2) : ");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Saisissez un nombre (3) : ");
+            c = double.Parse(Console.ReadLine());
+
+            petit = Math.Min(a, b);
+
+            Console.WriteLine("Le plus petit nombre est : " + Math.Min(petit, c));
+            Console.ReadLine();
+        }
+    }
+}
